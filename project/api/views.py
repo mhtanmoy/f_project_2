73dacf3ef2c8ebd07c042fb32eb44ca7ef59e8f6
Farhana islam urmi <76726981+Farhanaurmi@users.noreply.github.com>
2021-05-05 20:19:24 +0600

    added api stuff

--- project/api/views.py
+++ project/api/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics ,  permissions, mixins, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from app.models import *
+from .Serializers import *
 from django.http import HttpResponse
 
-# Create your views here.
 def home(request):
-    return HttpResponse('hello')
+    return HttpResponse("hello")
+
+
+class DriverList(generics.ListCreateAPIView):
+    queryset=Driver.objects.all()
+    serializer_class=DriverSerializer
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save()
