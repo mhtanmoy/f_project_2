adb37e93637dcb289cc312da74b170dc6b919633
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-05-08 15:24:02 +0600

    admin permission added

--- project/app/decorators.py
+++ project/app/decorators.py
@@ -9,3 +9,20 @@ def unauthenticated_user(view_func):
 		else:
 			return view_func(request, *args, **kwargs)
 	return wrapper_func
+
+def manager_only(view_func):
+	def wrapper_function(request, *args, **kwargs):
+		group = None
+		if request.user.groups.exists():
+			group = request.user.groups.all()[0].name
+
+		if group == 'staff':
+			return redirect('home')
+
+		if group == 'manager':
+			return view_func(request, *args, **kwargs)
+		
+		if group==None:
+			return HttpResponse('You are not authorized to view this page')
+
+	return wrapper_function
--- project/app/views.py
+++ project/app/views.py
@@ -13,26 +13,31 @@ def home(request):
     return render(request,'app/home.html')
 
 @login_required
+@manager_only
 def driver(request):
     drivers=Driver.objects.all()
     return render(request,'app/driver.html' , {'drivers':drivers})
 
 @login_required
+@manager_only
 def vehicle(request):
     vehicles=Vehicle.objects.all()
     return render(request,'app/vehicle.html' , {'vehicles':vehicles})
 
 @login_required
+@manager_only
 def bookinghistory(request):
     bookinghistorys=BookingHistory.objects.all()
     return render(request,'app/bookinghistory.html' , {'bookinghistorys':bookinghistorys})
 
 @login_required
+@manager_only
 def bookingdetails(request):
     bookingdetailss=BookingDetails.objects.all()
     return render(request,'app/bookingdetails.html' , {'bookingdetailss':bookingdetailss})
 
 @login_required
+@manager_only
 def customeruser(request):
     customerusers=CustomerUser.objects.all()
     return render(request,'app/customeruser.html' , {'customerusers':customerusers})
@@ -76,3 +81,5 @@ def signupuser(request):
 			login(request,admin)
 			return redirect('home')			
 	return render(request, 'app/signupuser.html',{'form':form} )
+
+    
